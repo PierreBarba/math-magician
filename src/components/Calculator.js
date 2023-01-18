14759d93337b23c05254fd9ee77fe9f1379de33d
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-01-18 01:30:00 -0600

    change class funtionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,39 @@
-function Calculator() {
-  return (
-    <div className="container-calculator">
-      <input className="calc-input" placeholder="0" type="text" />
-      <div className="line">
-        <button type="button" className="buttonC">AC</button>
-        <button type="button" className="buttonC">+/-</button>
-        <button type="button" className="buttonC">%</button>
-        <button type="button" className="buttonC color">÷</button>
-      </div>
-      <div className="line">
-        <button type="button" className="buttonC">7</button>
-        <button type="button" className="buttonC">8</button>
-        <button type="button" className="buttonC">9</button>
-        <button type="button" className="buttonC color">x</button>
-      </div>
-      <div className="line">
-        <button type="button" className="buttonC">4</button>
-        <button type="button" className="buttonC">5</button>
-        <button type="button" className="buttonC">6</button>
-        <button type="button" className="buttonC color">-</button>
-      </div>
-      <div className="line">
-        <button type="button" className="buttonC">1</button>
-        <button type="button" className="buttonC">2</button>
-        <button type="button" className="buttonC">3</button>
-        <button type="button" className="buttonC color">+</button>
-      </div>
-      <div className="line">
-        <button type="button" className="buttonC">0</button>
-        <button type="button" className="buttonC">.</button>
-        <button type="button" className="buttonC color">=</button>
-      </div>
-    </div>
-  );
+import React from "react";
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <section className="container-calculator">
+                <h3 className="result">0</h3>
+                <div className="buttonsCalculator">
+                    <button type="button" className="btnCalculator">AC</button>
+                    <button type="button" className="btnCalculator">+/-</button>
+                    <button type="button" className="btnCalculator">%</button>
+                    <button type="button" className="btnCalculator color-right">÷</button>
+                    <button type="button" className="btnCalculator">7</button>
+                    <button type="button" className="btnCalculator">8</button>
+                    <button type="button" className="btnCalculator">9</button>
+                    <button type="button" className="btnCalculator color-right">x</button>
+                    <button type="button" className="btnCalculator">4</button>
+                    <button type="button" className="btnCalculator">5</button>
+                    <button type="button" className="btnCalculator">6</button>
+                    <button type="button" className="btnCalculator color-right">-</button>
+                    <button type="button" className="btnCalculator">1</button>
+                    <button type="button" className="btnCalculator">2</button>
+                    <button type="button" className="btnCalculator">3</button>
+                    <button type="button" className="btnCalculator color-right">+</button>
+                    <button type="button" className="btnCalculator zero">0</button>
+                    <button type="button" className="btnCalculator">.</button>
+                    <button type="button" className="btnCalculator color-right">=</button>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Calculator;
